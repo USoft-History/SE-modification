1189d8ec33198534580a8c722cfb4abcaddbb139
javavirys <javavirys@gmail.com>
2026-05-30 23:41:21 +0300

    Add files

---
+++ Программы/Menu_Editor_0.5/Settings/blankmenu.ml
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<mml ver="1.0">
+  <menu id="Wilma_Generic_Id">
+    <element id="MainMenu">
+      <menu id="MainMenu" layout="DesktopTopMenuGFX2">
+        <accelerator>MapToKeypad</accelerator>
+        <highlight>4</highlight>
+      </menu>
+    </element>
+  </menu>
+</mml>
